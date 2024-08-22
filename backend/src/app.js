ec44da87b17ba718ba5078f5c69aa924a3a43e6b
Amir Bibi <amirbi595@gmail.com>
2024-08-22 13:41:27 +0300

    Update app.js cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,20 +6,25 @@ import { fetchMetaDataFromUrls } from "./controllers/urlController.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["https://*.amirs-projects-06b8a9b4.vercel.app"],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
-app.use(
-  helmet({
-    crossOriginResourcePolicy: { policy: "cross-origin" },
-  })
-);
-
+app.use(helmet());
 app.use(express.json());
 
 const limiter = rateLimit({
