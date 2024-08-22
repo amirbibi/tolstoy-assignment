106f13886b0e6646cac7e532f15fc08852cd7aa4
Amir Bibi <amirbi595@gmail.com>
2024-08-22 13:56:07 +0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://*.amirs-projects-06b8a9b4.vercel.app"],
+    origin: [
+      "https://tolstoy-assignment-3j6lgsewm-amirs-projects-06b8a9b4.vercel.app",
+    ],
   })
 );
 
